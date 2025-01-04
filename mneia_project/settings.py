492c5186f570f632d4ea1e23f9cb18c1a75af277
Marios Zindilis <marios@zindilis.com>
2025-01-04 08:40:32 +0000

    Added linters and fixed errors

--- mneia_project/settings.py
+++ mneia_project/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ytbtw=2fye4kg1owo79g+a#=r6l0*=o2#bwkcj(%_llw#!xmgo"
+SECRET_KEY_PATH = Path.home() / "Mneia" / "django-secret"
+SECRET_KEY = SECRET_KEY_PATH.read_text().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
